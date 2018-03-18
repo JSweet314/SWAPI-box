8c0efe6508c97d22f02dca784ab50a7e58199320
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-03-18 11:32:08 -0600

    Add fetch to App for ScrollingText data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,36 @@ import Main from '../Main/Main';
 import ScrollingText from '../ScrollingText/ScrollingText';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      openingCrawl: '',
+      movieTitle: '',
+      releaseDate: ''
+    };
+  }
+  componentDidMount() {
+    fetch('https://swapi.co/api/films/1/?format=json')
+      .then(response => response.json())
+      .then(SWAPIData => {
+        this.setState({
+          openingCrawl: SWAPIData.opening_crawl,
+          movieTitle: SWAPIData.title,
+          releaseDate: SWAPIData.release_date
+        });
+      });
+  }
+
+
   render() {
     return (
       <div className="app">
         <Header />
         <Main />
-        <ScrollingText />
+        <ScrollingText 
+          openingCrawl={this.state.openingCrawl}
+          movieTitle={this.state.movieTitle}
+          releaseDate={this.state.releaseDate}/>
       </div>
     );
   }
--- src/components/ScrollingText/ScrollingText.js
+++ src/components/ScrollingText/ScrollingText.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './ScrollingText.css';
+import PropTypes from 'prop-types';
 
-const ScrollingText = () => {
+const ScrollingText = ({openingCrawl, movieTitle, releaseDate}) => {
   return (
     <aside className="scrolling-text">
-      some movie text
+      {openingCrawl}
+      {movieTitle}
+      {releaseDate}
     </aside>
   );
+};
+
+ScrollingText.propTypes = {
+  openingCrawl: PropTypes.string.isRequired,
+  movieTitle: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired
 };
 
 export default ScrollingText;
