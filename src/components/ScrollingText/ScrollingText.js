f467b73943ea26ca5edba99848c539c6cab0bed2
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-03-18 10:39:14 -0600

    Add ScrollingText component and grid display to App

---
+++ src/components/ScrollingText/ScrollingText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ScrollingText.css';
+
+const ScrollingText = () => {
+  return (
+    <aside className="scrolling-text">
+      some movie text
+    </aside>
+  );
+};
+
+export default ScrollingText;
---
+++ src/components/ScrollingText/ScrollingText.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ScrollingText from './ScrollingText';
+import { shallow } from 'enzyme';
+
+describe('ScrollingText', () => {
+  it('should match a snapshot', () => {
+
+  });
+});
