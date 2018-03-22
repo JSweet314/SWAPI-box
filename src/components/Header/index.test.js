33b61dbd3306374f8f58011798cdfd0dcb70055f
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-03-21 23:13:49 -0600

    Add remaining component tests

--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -3,13 +3,8 @@ import Header from './index';
 import {shallow} from 'enzyme';
 
 describe('Header', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<Header numberOfFavorites={0} />);
-  });
-
   it('should match a snapshot', () => {
+    const wrapper = shallow(<Header numberOfFavorites={0} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
