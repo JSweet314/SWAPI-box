b973f5616097bf21c5e4c8ac169e4f15b08523e0
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-03-23 18:42:46 -0600

    Add localStorage mock

--- src/containers/OpeningCrawlContainer/index.test.js
+++ src/containers/OpeningCrawlContainer/index.test.js
@@ -3,11 +3,30 @@ import CrawlContainer from './index';
 import { shallow } from 'enzyme';
 
 describe('ScrollingText', () => {
-  it('should match a snapshot', () => {
+  it('should match a snapshot with empty state', () => {
     const wrapper = shallow(
       <CrawlContainer />, 
       {disableLifecycleMethods: true}
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match a snapshot with full state', () => {
+    const wrapper = shallow(
+      <CrawlContainer />,
+      { disableLifecycleMethods: true }
+    );
+    wrapper.setState({
+      openingCrawl: "I am placeholder text!",
+      title: "A StarWars Movie",
+      releaseDate: "2018-03-23"
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+  /*eslint-disable max-len*/
+  it('should have a method findClosestOpeningCrawl which sets state with localStorage data if available', () => {
+  /*eslint-enable max-len*/
+    
   });
 });
